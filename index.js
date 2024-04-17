f6b946fcff6031ab3da9bfe8d874da7686f00134
Samantha Pomeroy <samanthapomeroy38@gmail.com>
2024-04-16 20:04:05 -0500

    revised

--- index.js
+++ index.js
@@ -1,59 +1,65 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const logger = require('morgan')
+const littleid = require('littleid');
 const app = express();
-const {MongoClient} = require('mongodb');
-const urlparser = require('url')
-const dns = require('dns')
-
-const client = new MongoClient(process.env.MONGO_URL)
-const db = client.db('urlshortener')
-const urls = db.collection('urls')
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(cors());
+app.use(express.urlencoded({extended: true})
+);
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(logger('dev'))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-// app.get('/new', (req, res)=>{
-//   console.log('hello')
-// })
-// Your first API endpoint
-app.post('/api/shorturl', function(req, res) {
-  console.log(req.body)
-  const url = req.body.url
-  const dnslookup = dns.lookup(urlparser.parse(url).hostname,
-async (err, address)=>{
-  if(!address){
-    res.json({error: 'Invalid URL'})
-  }else {
 
-    const urlCount = await urls.countDocuments({})
-    const urlDoc = {
-      url, 
-      short_url: urlCount
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+let urlArray = []
+app.post('/api/shorturl', function(req, res) {
+    try {
+      
+      const url = req.body.url
+      let valid = url.startsWith('http://') || url.startsWith('https://')
+      if(!valid){
+        res.json({error: 'invalid url'})
+        return
+      }
+      let short_url = littleid()
+      let foundUrl = urlArray.find((item) => item.original_url === url)
+      if(foundUrl){
+        res.json({original_url: foundUrl.original_url, short_url: foundUrl.short_url})
+        return
+      }
+      else {
+        urlArray.push({original_url: url, short_url: short_url})
+        res.json({original_url: url, short_url: short_url})
+        return
+      }
+    } catch (error) {
+      res.json({error: 'invalid url'})
     }
-    const result = await urls.insertOne(urlDoc)
-    console.log(result)
-    res.json({original_url: url, short_url: urlCount})
+
+})
+
+app.get('/api/shorturl/:short_url?', function(req, res) {
+  try {
+    let found = urlArray.find((item) => item.short_url === req.params.short_url)
+    if(found){
+      res.redirect(found.original_url)
+      return
+    }
+  } catch (error) {
+    res.json({error: 'invalid url'})
 
   }
-})
-});
-
-app.get('/api/short_url/:short_url', async (req, res)=>{
-  const shorturl = req.params.short_url
-  const urlDoc = await urls.findOne({short_url: +shorturl})
-  res.redirect(urlDoc.url)
 })
 
 app.listen(port, function() {
